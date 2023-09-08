5e734f1700a5cf93a99bd4ec6bba0c45b35ddf18
Aniket Dumbre <anktd23@gmail.com>
2023-09-08 23:21:56 +0530

    updated

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ if __name__ =="__main__":
     for input_file_name in os.listdir(file_path_input):
         if input_file_name.endswith('.pdf'):
             input_file_path = os.path.join(file_path_input, input_file_name)
-            #text = textExtract(input_file_path)
-            text = textExtractPyPDF(input_file_path)
+            text = textExtract(input_file_path)
+            #text = textExtractPyPDF(input_file_path)
             output_text_file = os.path.splitext(input_file_name)[0] + '.txt'
             saveTextToFile(text, file_path_output, output_text_file)
     txtFileMerger(file_path_output, file_path_merged )
